5d8aecb2364e7c063f655028c913d67d12e7a6f6
berney <xkongdadi@gmail.com>
2025-12-06 21:28:01 +0800

    implement echo

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,29 @@ int main() {
   {
     std::cout << "$ ";
     std::string command;
-  std::getline(std::cin,command);
-  if (command == "exit"){
-    break;
-  }
-  std::cout<<command<<": command not found"<<std::endl;
+    std::getline(std::cin,command);
+    if (command == "exit"){
+      break;
+    }
+    size_t space_pos=command.find(" ");
+    if(space_pos!=std::string::npos){
+      std::string order=command.substr(0,space_pos);
+      std::string arguments = command.substr(space_pos + 1);
+      // 逻辑判断
+        if (order == "echo") {
+            std::cout << arguments << std::endl;
+        }
+    }else{
+      if (command == "echo"){
+        std::cout << "" << std::endl; // echo 空内容只打印一个换行
+      }else {
+            std::cout << command << ": command not found" << std::endl;
+      }
+    }
+   
   
   }
+  return 0;
   
   
 }
