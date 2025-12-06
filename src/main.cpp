ae241bad7cf3bd954c571e90d9868822412e7945
berney <xkongdadi@gmail.com>
2025-12-06 20:58:40 +0800

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -8,4 +8,7 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+  std::string command;
+  std::getline(std::cin,command);
+  std::cout<<command<<": command not found"<<std::endl;
 }
