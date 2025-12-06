cd0360a96355c1f7fbdecf31c033428e2d793bcd
berney <xkongdadi@gmail.com>
2025-12-06 21:19:26 +0800

    implement exit

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,11 @@ int main() {
     std::cout << "$ ";
     std::string command;
   std::getline(std::cin,command);
+  if (command == "exit"){
+    break;
+  }
   std::cout<<command<<": command not found"<<std::endl;
+  
   }
   
   
